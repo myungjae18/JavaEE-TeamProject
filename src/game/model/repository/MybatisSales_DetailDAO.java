a1a672b73cd3d6ee666c6ff2b9b3d8bc9ecd74c8
myungjae18 <01087153603a@gmail.com>
2019-05-22 18:21:19 +0900

    Project - final version

---
+++ src/game/model/repository/MybatisSales_DetailDAO.java
@@ -0,0 +1,31 @@
+package game.model.repository;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import game.model.domain.Sales_Detail;
+
+@Repository
+public class MybatisSales_DetailDAO implements Sales_DetailDAO {
+   @Autowired
+   private SqlSessionTemplate sessionTemplate;
+
+   public int insert(Sales_Detail sales_Detail) {
+      return sessionTemplate.insert("Sales_Detail.insert", sales_Detail);
+   }
+
+   public List selectAll() {
+      return sessionTemplate.selectList("Sales_Detail.selectAll");
+   }
+   
+   public List selectByGame(int game_id) {
+      return sessionTemplate.selectList("Sales_Detail.selectByGame", game_id);
+   }
+   
+   public int countGame(int game_id) {
+	   return sessionTemplate.selectOne("Sales_Detail.countGame", game_id);
+   }
+}
